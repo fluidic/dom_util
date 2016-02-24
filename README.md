7482cb2aade5e16beaa07c4cbf45355d6de1115b
Kwang Yul Seo <kwangyul.seo@gmail.com>
2016-02-24 21:03:04 +0900

    Initial import.

---
+++ README.md
@@ -0,0 +1,9 @@
+# dom_util
+
+Utilities for working with DOM.
+
+## Features and bugs
+
+Please file feature requests and bugs at the [issue tracker][tracker].
+
+[tracker]: https://github.com/fluidic/dom_util/issues
---
+++ example/dom_util_example.dart
@@ -0,0 +1,11 @@
+// Copyright (c) 2016, <your name>. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+library dom_util.example;
+
+import 'package:dom_util/dom_util.dart';
+
+main() {
+  var awesome = new Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
