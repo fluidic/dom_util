324e7f78300b51717db41376b92b2732ec422a8d
Kwang Yul Seo <kwangyul.seo@gmail.com>
2016-02-24 21:05:06 +0900

    Add dom_util.
    The dom_util library is extracted from seed.

--- lib/src/dom_util_base.dart
+++ lib/src/dom_util_base.dart
@@ -1,11 +1,45 @@
 // Copyright (c) 2016, <your name>. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-// TODO: Put public facing types in this file.
-
 library dom_util.base;
 
-/// Checks if you are awesome. Spoiler: you are.
-class Awesome {
-  bool get isAwesome => true;
+import 'dart:html';
+
+import 'package:disposable/disposable.dart';
+
+/// Throttles the resize event using requestAnimationFrame since resize
+/// events can fire at a high rate.
+Disposable onResize(Element element, void callback()) {
+  var elementWidth = element.style.width;
+  var elementHeight = element.style.height;
+  void checkForChanges() {
+    if (elementWidth != element.style.width ||
+        elementHeight != element.style.height) {
+      elementWidth = element.style.width;
+      elementHeight = element.style.height;
+      callback();
+    }
+  }
+
+  var running = false;
+  void debounce() {
+    if (running) return;
+    running = true;
+    window.animationFrame.whenComplete(() {
+      checkForChanges();
+      running = false;
+    });
+  }
+
+  final disposables = new CompositeDisposable();
+
+  final observer = new MutationObserver((mutations, observer) => debounce());
+  observer.observe(document.body,
+      attributes: true, childList: true, characterData: true, subtree: true);
+  disposables.add(new Disposable.create(observer.disconnect));
+
+  final subscription = window.onResize.listen((_) => debounce());
+  disposables.add(new Disposable.from(subscription));
+
+  return disposables;
 }
